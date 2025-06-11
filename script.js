bdc544634c46e126702b3e697ad6e24006574f8b
WerAle <negocioswer@gmail.com>
2025-06-11 17:10:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ window.addEventListener('DOMContentLoaded', () => {
   const intro = document.querySelector('.intro');
   const musica = document.getElementById('musica');
   const conteudo = document.getElementById('conteudo');
-  const iniciais = document.querySelector('.iniciais');
 
   selo.addEventListener('click', () => {
     selo.classList.add('fade-out');
-    iniciais.classList.add('fade-out');
 
     setTimeout(() => {
       intro.classList.add('oculto');
@@ -16,9 +14,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
       if (musica) {
         musica.play().catch(error => {
-          console.log('Erro ao reproduzir a música:', error);
+          console.warn('Erro ao reproduzir a música:', error);
         });
       }
-    }, 1000); // tempo da animação
+    }, 1000); // Espera o selo sumir antes de mostrar o conteúdo
   });
 });
