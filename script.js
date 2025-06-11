03acfd537497d7f108d5989e4cfa0caae1763501
WerAle <negocioswer@gmail.com>
2025-06-10 22:39:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const selo = document.getElementById("selo");
+  const conteudo = document.getElementById("conteudo");
+  const intro = document.querySelector(".intro");
+
+  selo.addEventListener("click", () => {
+    intro.classList.add("fade-out");
+
+    setTimeout(() => {
+      intro.style.display = "none";
+      conteudo.classList.remove("oculto");
+      document.body.classList.add("fundo-espelho");
+    }, 1000); // tempo da animação de fade-out
+  });
+});
