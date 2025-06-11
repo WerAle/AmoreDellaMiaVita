23d3a170cb080dd94c325794b4b40998f857aed7
WerAle <negocioswer@gmail.com>
2025-06-11 16:49:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 window.addEventListener('DOMContentLoaded', () => {
   const selo = document.getElementById('selo');
   const intro = document.querySelector('.intro');
-  const conteudo = document.getElementById('conteudo');
   const musica = document.getElementById('musica');
+  const conteudo = document.getElementById('conteudo');
+  const iniciais = document.querySelector('.iniciais');
 
   selo.addEventListener('click', () => {
     selo.classList.add('fade-out');
-
-    if (musica) {
-      musica.play().catch(error => {
-        console.log('Erro ao reproduzir a música:', error);
-      });
-    }
+    iniciais.classList.add('fade-out');
 
     setTimeout(() => {
-      intro.style.display = 'none';
+      intro.classList.add('oculto');
       conteudo.classList.remove('oculto');
       document.body.classList.add('fundo-espelho');
+
+      if (musica) {
+        musica.play().catch(error => {
+          console.log('Erro ao reproduzir a música:', error);
+        });
+      }
     }, 1000); // tempo da animação
   });
 });
